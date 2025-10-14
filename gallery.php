a6748fde661c190f67612f37645839dfc2e56dc3
Abiola Yakubu <yakubuabiola2003@gmail.com>
2025-10-14 23:52:36 +0100

    ..

---
+++ gallery.php
@@ -0,0 +1,109 @@
+<?php include('header.php'); ?>
+    <div class="breadcumb-wrapper" data-bg-src="assets/img/breadcumb/breadcumb-bg-3.jpg">
+        <div class="container z-index-common">
+            <div class="breadcumb-content">
+                <h1 class="breadcumb-title">Our <span class="inner-text">Portfolio</span></h1>
+                <div class="breadcumb-menu-wrap">
+                    <ul class="breadcumb-menu">
+                        <li><a href="index.html">Home</a></li>
+                        <li>Our <span class="inner-text">Portfolio</span></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    <section class="space">
+        <div class="container">
+            <div class="row gy-gx filter-active">
+                <div class="col-md-6 col-xxl-auto filter-item">
+                    <div class="gallery-style1">
+                        <div class="gallery-img"><img src="assets/img/gallery/gal-4-1.jpg" alt="Gallery Image" class="w-100"></div>
+                        <div class="gallery-shape" data-overlay="white" data-opacity="9"></div>
+                        <div class="gallery-content"><a href="assets/img/gallery/gal-4-1.jpg" class="gallery-btn popup-image"><i class="fal fa-plus"></i></a>
+                            <h3 class="gallery-title"><a href="gallery-details.html" class="text-inherit">Thai Massage</a></h3><span class="gallery-tag">Massage</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-xxl-auto filter-item">
+                    <div class="gallery-style1">
+                        <div class="gallery-img"><img src="assets/img/gallery/gal-4-2.jpg" alt="Gallery Image" class="w-100"></div>
+                        <div class="gallery-shape" data-overlay="white" data-opacity="9"></div>
+                        <div class="gallery-content"><a href="assets/img/gallery/gal-4-2.jpg" class="gallery-btn popup-image"><i class="fal fa-plus"></i></a>
+                            <h3 class="gallery-title"><a href="service-details.html" class="text-inherit">Body Massage</a></h3><span class="gallery-tag">Beauty</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-xxl-auto filter-item">
+                    <div class="gallery-style1">
+                        <div class="gallery-img"><img src="assets/img/gallery/gal-4-3.jpg" alt="Gallery Image" class="w-100"></div>
+                        <div class="gallery-shape" data-overlay="white" data-opacity="9"></div>
+                        <div class="gallery-content"><a href="assets/img/gallery/gal-4-3.jpg" class="gallery-btn popup-image"><i class="fal fa-plus"></i></a>
+                            <h3 class="gallery-title"><a href="service-details.html" class="text-inherit">Skin Care</a></h3><span class="gallery-tag">Expert</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-xxl-auto filter-item">
+                    <div class="gallery-style1">
+                        <div class="gallery-img"><img src="assets/img/gallery/gal-4-4.jpg" alt="Gallery Image" class="w-100"></div>
+                        <div class="gallery-shape" data-overlay="white" data-opacity="9"></div>
+                        <div class="gallery-content"><a href="assets/img/gallery/gal-4-4.jpg" class="gallery-btn popup-image"><i class="fal fa-plus"></i></a>
+                            <h3 class="gallery-title"><a href="service-details.html" class="text-inherit">Health Care</a></h3><span class="gallery-tag">Beauty</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-xxl-auto filter-item">
+                    <div class="gallery-style1">
+                        <div class="gallery-img"><img src="assets/img/gallery/gal-4-5.jpg" alt="Gallery Image" class="w-100"></div>
+                        <div class="gallery-shape" data-overlay="white" data-opacity="9"></div>
+                        <div class="gallery-content"><a href="assets/img/gallery/gal-4-5.jpg" class="gallery-btn popup-image"><i class="fal fa-plus"></i></a>
+                            <h3 class="gallery-title"><a href="service-details.html" class="text-inherit">Face Care</a></h3><span class="gallery-tag">Lifestyle</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-xxl-auto filter-item">
+                    <div class="gallery-style1">
+                        <div class="gallery-img"><img src="assets/img/gallery/gal-4-6.jpg" alt="Gallery Image" class="w-100"></div>
+                        <div class="gallery-shape" data-overlay="white" data-opacity="9"></div>
+                        <div class="gallery-content"><a href="assets/img/gallery/gal-4-6.jpg" class="gallery-btn popup-image"><i class="fal fa-plus"></i></a>
+                            <h3 class="gallery-title"><a href="service-details.html" class="text-inherit">Eye litelime</a></h3><span class="gallery-tag">Classic</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-xxl-auto filter-item">
+                    <div class="gallery-style1">
+                        <div class="gallery-img"><img src="assets/img/gallery/gal-4-7.jpg" alt="Gallery Image" class="w-100"></div>
+                        <div class="gallery-shape" data-overlay="white" data-opacity="9"></div>
+                        <div class="gallery-content"><a href="assets/img/gallery/gal-4-7.jpg" class="gallery-btn popup-image"><i class="fal fa-plus"></i></a>
+                            <h3 class="gallery-title"><a href="service-details.html" class="text-inherit">Ruler Massage</a></h3><span class="gallery-tag">Queen</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-xxl-auto filter-item">
+                    <div class="gallery-style1">
+                        <div class="gallery-img"><img src="assets/img/gallery/gal-4-8.jpg" alt="Gallery Image" class="w-100"></div>
+                        <div class="gallery-shape" data-overlay="white" data-opacity="9"></div>
+                        <div class="gallery-content"><a href="assets/img/gallery/gal-4-8.jpg" class="gallery-btn popup-image"><i class="fal fa-plus"></i></a>
+                            <h3 class="gallery-title"><a href="service-details.html" class="text-inherit">Nose Massage</a></h3><span class="gallery-tag">Massage</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <div class="bg-light-2 space-extra">
+        <div class="shape-mockup jump-reverse-img d-none d-xl-block" data-left="2%" data-top="10%"><img src="assets/img/shape/b-s-1-3.png" alt="shape"></div>
+        <div class="shape-mockup jump-img d-none d-xl-block" data-right="2%" data-bottom="8%"><img src="assets/img/shape/b-s-1-4.png" alt="shape"></div>
+        <div class="container">
+            <div class="row vs-carousel text-center" data-slide-show="6" data-lg-slide-show="4" data-md-slide-show="3" data-sm-slide-show="2" data-xs-slide-show="2">
+                <div class="col-auto"><img src="assets/img/brand/b-1-1.png" alt="brand"></div>
+                <div class="col-auto"><img src="assets/img/brand/b-1-2.png" alt="brand"></div>
+                <div class="col-auto"><img src="assets/img/brand/b-1-3.png" alt="brand"></div>
+                <div class="col-auto"><img src="assets/img/brand/b-1-4.png" alt="brand"></div>
+                <div class="col-auto"><img src="assets/img/brand/b-1-5.png" alt="brand"></div>
+                <div class="col-auto"><img src="assets/img/brand/b-1-6.png" alt="brand"></div>
+                <div class="col-auto"><img src="assets/img/brand/b-1-3.png" alt="brand"></div>
+            </div>
+        </div>
+    </div>
+    
+<?php include('footer.php'); ?>
--- header.php
+++ header.php
@@ -29,11 +29,12 @@
 </head>
 
 <body>
-    <div class="preloader"><button class="vs-btn preloaderCls">Cancel Preloader</button>
+    <!-- <div class="preloader"><button class="vs-btn preloaderCls">Cancel Preloader</button>
         <div class="preloader-inner">
             <div class="loader"></div>
         </div>
-    </div><svg viewBox="0 0 150 150" class="svg-hidden">
+    </div> -->
+    <svg viewBox="0 0 150 150" class="svg-hidden">
         <path id="textPath" d="M 0,75 a 75,75 0 1,1 0,1 z"></path>
     </svg>
     <div class="vs-menu-wrapper">
